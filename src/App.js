c35d1ec2556f31b2fc58bedd836a90a50d635b5d
Mohammad M. Ahmad <mohammad.moaaz.ahmad@gmail.com>
2019-03-05 22:29:34 -0600

    Set up Material-UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import 'typeface-roboto';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        {/* <header className="App-header"> */}
+        <div class=''>
+
+        </div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -18,8 +30,8 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
+          </a> */}
+        {/* </header> */}
       </div>
     );
   }
